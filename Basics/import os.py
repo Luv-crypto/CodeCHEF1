69e7b35db85d41da2d62cfb3dfa00b9576210770
Love Fadia <lfadia11@gmail.com>
2022-04-19 14:17:23 +0530

    Delete Basics directory

--- Basics/import os.py
+++
@@ -1,7 +0,0 @@
-import re
-datePattern = re.compile(r"""^(.*?) 
-((0|1)?\d)- 
-((0|1|2|3)?\d)- 
-((19|20)\d\d) 
-(.*?)$ 
-w """, re.VERBOSE)
--- Basics/open.py
+++
@@ -1,7 +0,0 @@
-#! usr/bin/python 
- 
- 
-fo = open("bjd.txt")
-print ("name of the file:" , fo.name)
-
-fo.close()
